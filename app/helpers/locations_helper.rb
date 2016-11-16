33803315b4214b1101ff94abb1e386faec293236
Keith Clark <geauxlsutigers24@gmail.com>
2016-11-16 17:04:25 +0000

    added logic for api fetching and buses

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,2 +1,14 @@
 module LocationsHelper
+
+  def fetch_api_data(source)
+    http = Net::HTTP.get_resource(URI.parse(source))
+    data = http.body
+    return JSON.parse(data)
+  end
+
+  def is_nearby(lat_user, long_user, lat_bus, long_bus)
+    (long_user - long_bus).abs  <= 0.01 && (lat_user - lat_bus).abs <= 0.01
+
+  end
+
 end
